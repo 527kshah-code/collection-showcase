bdeb2c7f0aefcc1f405dacb071a29e4dc279d853
Kavya <527kshah@frhsd.com>
2026-05-27 03:03:17 +0000

    started the skz data on the 4th album

---
+++ js/skz-data.js
@@ -0,0 +1,322 @@
+const songs = [
+    {
+        songName: "MEGAVERSE",
+        releaseDate: "November 10, 2023",
+        albumName: "Rock-Star",
+        runtime: "3:05",
+        image: "imgs/1.jpg",
+        fact: "..."
+    },
+    {
+        songName: "LALALALA",
+        releaseDate: "November 10, 2023",
+        albumName: "Rock-Star",
+        runtime: "3:02",
+        image: "imgs/1.jpg",
+        fact: "..."
+    },
+    {
+        songName: "BLIND SPOT",
+        releaseDate: "November 10, 2023",
+        albumName: "Rock-Star",
+        runtime: "3:20",
+        image: "imgs/1.jpg",
+        fact: "..."
+    },
+    {
+        songName: "COMFLEX",
+        releaseDate: "November 10, 2023",
+        albumName: "Rock-Star",
+        runtime: "2:52",
+        image: "imgs/1.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Cover Me",
+        releaseDate: "November 10, 2023",
+        albumName: "Rock-Star",
+        runtime: "3:11",
+        image: "imgs/1.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Leave",
+        releaseDate: "November 10, 2023",
+        albumName: "Rock-Star",
+        runtime: "3:39",
+        image: "imgs/1.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Social Path (feat. LiSA)(Korean Ver.)",
+        releaseDate: "November 10, 2023",
+        albumName: "Rock-Star",
+        runtime: "3:17",
+        image: "imgs/1.jpg",
+        fact: "..."
+    },
+    {
+        songName: "LALALALA (Rock Ver.)",
+        releaseDate: "November 10, 2023",
+        albumName: "Rock-Star",
+        runtime: "3:07",
+        image: "imgs/1.jpg",
+        fact: "..."
+    },
+    {
+        songName: "The Tortoise and the Hare",
+        releaseDate: "September 14, 2020",
+        albumName: "In Life",
+        runtime: "3:44",
+        image: "imgs/2.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Back Door",
+        releaseDate: "September 14, 2020",
+        albumName: "In Life",
+        runtime: "3:09",
+        image: "imgs/2.jpg",
+        fact: "..."
+    },
+    {
+        songName: "B Me",
+        releaseDate: "September 14, 2020",
+        albumName: "In Life",
+        runtime: "3:25",
+        image: "imgs/2.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Any",
+        releaseDate: "September 14, 2020",
+        albumName: "In Life",
+        runtime: "2:49",
+        image: "imgs/2.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Ex",
+        releaseDate: "September 14, 2020",
+        albumName: "In Life",
+        runtime: "3:37",
+        image: "imgs/2.jpg",
+        fact: "..."
+    },
+    {
+        songName: "We Go (Bangchan, Changbin, Han)",
+        releaseDate: "September 14, 2020",
+        albumName: "In Life",
+        runtime: "2:37",
+        image: "imgs/2.jpg",
+        fact: "..."
+    },
+    {
+        songName: "WOW (Lee Know, Felix, Hyunjin)",
+        releaseDate: "September 14, 2020",
+        albumName: "In Life",
+        runtime: "3:13",
+        image: "imgs/2.jpg",
+        fact: "..."
+    },
+    {
+        songName: "My Universe (Seungmin, I.N)",
+        releaseDate: "September 14, 2020",
+        albumName: "In Life",
+        runtime: "3:23",
+        image: "imgs/2.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Easy",
+        releaseDate: "September 14, 2020",
+        albumName: "In Life",
+        runtime: "3:03",
+        image: "imgs/2.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Pacemaker",
+        releaseDate: "September 14, 2020",
+        albumName: "In Life",
+        runtime: "3:10",
+        image: "imgs/2.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Airplane",
+        releaseDate: "September 14, 2020",
+        albumName: "In Life",
+        runtime: "3:34",
+        image: "imgs/2.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Another Day",
+        releaseDate: "September 14, 2020",
+        albumName: "In Life",
+        runtime: "2:46",
+        image: "imgs/2.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Phobia",
+        releaseDate: "September 14, 2020",
+        albumName: "In Life",
+        runtime: "3:32",
+        image: "imgs/2.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Blueprint",
+        releaseDate: "September 14, 2020",
+        albumName: "In Life",
+        runtime: "4:11",
+        image: "imgs/2.jpg",
+        fact: "..."
+    },
+    {
+        songName: "TA",
+        releaseDate: "September 14, 2020",
+        albumName: "In Life",
+        runtime: "3:28",
+        image: "imgs/2.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Heaven",
+        releaseDate: "September 14, 2020",
+        albumName: "In Life",
+        runtime: "3:19",
+        image: "imgs/2.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Go LIVE",
+        releaseDate: "June 17, 2020",
+        albumName: "Go Live",
+        runtime: "1:50",
+        image: "imgs/3.jpg",
+        fact: "..."
+    },
+    {
+        songName: "God's Menu",
+        releaseDate: "June 17, 2020",
+        albumName: "Go Live",
+        runtime: "2:47",
+        image: "imgs/3.jpg",
+        fact: "..."
+    },
+    {
+        songName: "TOP ('Tower of God' OP)",
+        releaseDate: "June 17, 2020",
+        albumName: "Go Live",
+        runtime: "1:50",
+        image: "imgs/3.jpg",
+        fact: "..."
+    },
+    {
+        songName: "SLUMP ('Tower of God' ED)",
+        releaseDate: "June 17, 2020",
+        albumName: "Go Live",
+        runtime: "2:14",
+        image: "imgs/3.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Mixtape: Gone Days",
+        releaseDate: "June 17, 2020",
+        albumName: "Go Live",
+        runtime: "3:14",
+        image: "imgs/3.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Mixtape: On Track",
+        releaseDate: "June 17, 2020",
+        albumName: "Go Live",
+        runtime: "3:28",
+        image: "imgs/3.jpg",
+        fact: "..."
+    },
+    {
+        songName: "CHEESE",
+        releaseDate: "August 23, 2021",
+        albumName: "No Easy",
+        runtime: "3:02",
+        image: "imgs/4.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Thunderous",
+        releaseDate: "August 23, 2021",
+        albumName: "No Easy",
+        runtime: "3:03",
+        image: "imgs/4.jpg",
+        fact: "..."
+    },
+    {
+        songName: "DOMINO",
+        releaseDate: "August 23, 2021",
+        albumName: "No Easy",
+        runtime: "3:18",
+        image: "imgs/4.jpg",
+        fact: "..."
+    },
+    {
+        songName: "SSICK",
+        releaseDate: "August 23, 2021",
+        albumName: "No Easy",
+        runtime: "3:10",
+        image: "imgs/4.jpg",
+        fact: "..."
+    },
+    {
+        songName: "The View",
+        releaseDate: "August 23, 2021",
+        albumName: "No Easy",
+        runtime: "3:21",
+        image: "imgs/4.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Sorry, I Love You",
+        releaseDate: "August 23, 2021",
+        albumName: "No Easy",
+        runtime: "2:57",
+        image: "imgs/4.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Silent Cry",
+        releaseDate: "August 23, 2021",
+        albumName: "No Easy",
+        runtime: "3:29",
+        image: "imgs/4.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Secret Secret",
+        releaseDate: "August 23, 2021",
+        albumName: "No Easy",
+        runtime: "3:29",
+        image: "imgs/4.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Star Lost",
+        releaseDate: "August 23, 2021",
+        albumName: "No Easy",
+        runtime: "3:35",
+        image: "imgs/4.jpg",
+        fact: "..."
+    },
+    {
+        songName: "Red Lights (Bangchan, Hyunjin)",
+        releaseDate: "August 23, 2021",
+        albumName: "No Easy",
+        runtime: "3:09",
+        image: "imgs/4.jpg",
+        fact: "..."
+    },
+]
