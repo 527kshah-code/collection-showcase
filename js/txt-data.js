9e5790302c716a267851b4dabb22104a6fe546c1
Kavya <527kshah@frhsd.com>
2026-06-02 03:38:12 +0000

    started enhyphen page

---
+++ js/txt-data.js
@@ -0,0 +1,10 @@
+const songs = [
+    {
+        songName: "...",
+        releaseDate: " , 20",
+        albumName: "...",
+        runtime: ":",
+        image: "imgs/2.png",
+        fact: "..."
+    },
+]
